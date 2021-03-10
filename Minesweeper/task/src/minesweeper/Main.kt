662fe07310565142094ab2af35573afdb3294655
NikoBolt <docnikbolt@gmail.com>
2021-03-10 14:00:27 +0300

    Stage 1/5: Lay the groundwork

---
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -0,0 +1,15 @@
+package minesweeper
+/*
+Stage 1/5: Lay the groundwork
+ */
+fun main() {
+    print(  ".X.......\n" +
+            ".....X..X\n" +
+            "....X....\n" +
+            "......X..\n" +
+            "..X......\n" +
+            "....X....\n" +
+            "..X......\n" +
+            "..X......\n" +
+            "......X..")
+}
