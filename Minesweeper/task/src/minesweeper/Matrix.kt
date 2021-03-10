2518707daf9c07602ef0efb858a57b083a29abe7
NikoBolt <docnikbolt@gmail.com>
2021-03-10 18:04:08 +0300

    Stage 3/5: Look around you

--- Minesweeper/task/src/minesweeper/Matrix.kt
+++ Minesweeper/task/src/minesweeper/Matrix.kt
@@ -16,14 +16,36 @@ class Matrix {
 
     fun getElenent(row: Int, column: Int): Int = m[row][column]
 
+    fun setElement(row: Int, column: Int, nomber: Int) {
+        m[row][column] = nomber
+    }
+
     fun generateMines(minesCount: Int) {
         while (this.minesCount != minesCount) {
             var mine = arrayOf(Random.nextInt(0, 9), Random.nextInt(0, 9))
             if (getElenent(mine[0], mine[1]) != -1) {
                 this.minesCount++
-                m[mine[0]][mine[1]] = -1
+                setElement(mine[0], mine[1], -1)
+                addHints(mine)
             }
         }
+    }
+
+    private fun addHints(mine: Array<Int>) {
+        for (yy in mine[0] - 1..mine[0] + 1) {
+            if (yy >= 0 && yy < y) {
+                for (xx in mine[1] - 1..mine[1] + 1) {
+                    if (xx >= 0 && xx < x) {
+                        addMineHint(yy,xx)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addMineHint(yy: Int, xx: Int) {
+        if (getElenent(yy, xx) == -1) return
+        setElement(yy, xx, getElenent(yy, xx) + 1)
     }
 
     fun printField() {
@@ -32,6 +54,7 @@ class Matrix {
             for (xx in 0 until x) {
                 output += when (getElenent(yy, xx)) {
                     -1 -> "X"
+                    in 1..8 -> getElenent(yy, xx).toString()
                     else -> "."
                 }
             }
